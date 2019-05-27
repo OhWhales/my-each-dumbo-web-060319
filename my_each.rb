d1ac87cd61ba305782a8ff2e5ccf0001492e60f7
impartial-flow-3347 <brandon.lau86@gmail.com>
2019-05-27 22:17:59 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -4,7 +4,9 @@ def my_each(array) # put argument(s) here
     i = 0 
     
     while ( i < array.length ) 
-    yield(array[i])
+      yield(array[i])
+      puts array[i]
+      i = i+1
     
   
 end
